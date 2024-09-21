6dacc3ea3eb85a4d5bafdbbe7290fda9383117ea
Vu Gia Dai <vugiadai.93@gmail.com>
2024-09-21 10:12:55 +0700

    initial commit

---
+++ src/Daicode/HelloWorld.java
@@ -0,0 +1,10 @@
+package Daicode;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello PTIT.");
+		System.out.println("Hello PTIT.");
+		System.out.println("Hello PTIT.");
+	}
+
+}
